e3dd33a9d1e5706ed09d3c7b59846a3110dfc964
RatovosonPascal <lratovoson@gmail.com>
2024-09-14 12:08:42 +0200

    modification on code

---
+++ script.js
@@ -0,0 +1,23 @@
+// script.js
+let slideIndex = 0;
+let slides = document.querySelectorAll('.slides');
+
+function showSlides() {
+    // Masquer toutes les images
+    slides.forEach(slide => slide.classList.remove('active'));
+
+    // Passer à l'image suivante
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    // Afficher l'image active
+    slides[slideIndex - 1].classList.add('active');
+
+    // Répéter toutes les 3 secondes
+    setTimeout(showSlides, 3000);
+}
+
+// Lancer le diaporama automatiquement
+showSlides();
